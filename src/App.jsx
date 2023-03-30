2a389c57c836f5b12c95ffcf8a97f7a2cd1636b4
adel hamad <adel1971.adelal@metropolia.fi>
2023-03-30 10:29:22 +0300

    contexts

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import Home from './views/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from './views/Layout';
+import Single from './views/Single';
+import Profile from './views/Profile';
+import Login from './views/Login';
+import {MediaProvider} from './contexts/MediaContext';
+import Logout from './views/Logout';
+
+console.log('base', import.meta.env.BASE_URL);
+
+const App = () => {
+  return (
+    <Router basename={import.meta.env.BASE_URL}>
+      <MediaProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/single" element={<Single />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+        </Routes>
+      </MediaProvider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/MediaTable.jsx
@@ -0,0 +1,19 @@
+import {useMedia} from '../hooks/apiHooks';
+import MediaRow from './MediaRow';
+const MediaTable = () => {
+  const {mediaArray} = useMedia();
+
+  return (
+    <table>
+      <tbody>
+        {mediaArray.map((item, index) => {
+          return <MediaRow key={index} file={item} />;
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+MediaTable.propTypes = {};
+
+export default MediaTable;
