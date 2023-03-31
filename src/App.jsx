8396d7ddabff47d0fe07c1080225f7097d389da3
adel hamad <adel1971.adelal@metropolia.fi>
2023-03-31 10:39:38 +0300

    context

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,25 @@ import Single from './views/Single';
 import Profile from './views/Profile';
 import Login from './views/Login';
 
+import Logout from './views/Logout';
+import {MediaProvider} from './contexts/MediaContext';
+
 console.log('base', import.meta.env.BASE_URL);
 
 const App = () => {
   return (
     <Router basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/single" element={<Single />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
+      <MediaProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/single" element={<Single />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+        </Routes>
+      </MediaProvider>
     </Router>
   );
 };
