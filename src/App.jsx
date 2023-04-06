ad9da4069f79902e044e703ee708317f7e430ceb
adel hamad <adel1971.adelal@metropolia.fi>
2023-04-06 09:29:17 +0300

    upload start

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Layout from './views/Layout';
 import Single from './views/Single';
 import Profile from './views/Profile';
 import Login from './views/Login';
-
-import Logout from './views/Logout';
 import {MediaProvider} from './contexts/MediaContext';
+import Logout from './views/Logout';
+import Upload from './views/Upload';
 
 console.log('base', import.meta.env.BASE_URL);
 
@@ -21,6 +21,7 @@ const App = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/single" element={<Single />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/upload" element={<Upload />} />
             <Route path="/logout" element={<Logout />} />
           </Route>
         </Routes>
